59b191dbd7577aa32105bfb91fc1f93426763a0f
Arthuro <arthuro.morales@gmail.com>
2021-12-04 12:03:30 -0300

    agregue botones

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
+function piedra(player1){
+    alert("piedra");
+}
+
+function papel(){
+    alert("papel");
+}
+
+function tijera(){
+    alert("tijera");
+}
+
+
 function jugarPlayer1() {
     var player1;
     var opcion;
 
     while (typeof player1 == "undefined") {
-        opcion = prompt("Piedra, papel o tijera ? ");
+        
         opcion = opcion.toUpperCase();
 
         switch (opcion) {
@@ -30,6 +43,32 @@ var machine;
 var winner;
 var rondas = parseInt(prompt("Hola Player1, ¿cuantas rondas desea jugar? "));
 
+/* for (var i = 1; i <= rondas; i++) {
+
+    player1 = parseInt(jugarPlayer1());
+    machine = Math.floor(Math.random() * 3); */
+
+    switch (player1) {
+        case 0:         // Usuario eligio PIEDRA
+            switch (machine) {
+                case 0:     // Machine eligio PIEDRA
+                    winner = "Empate"
+                    alert("Empataste esta ronda");
+                    break;
+                case 1:     // Machine eligio PAPEL
+                    winner = "Player1"
+                    alert("Empataste esta ronda");
+                    break;
+                case 2:     // Machine eligio TIJERA
+                    winner = "Machine"
+                    alert("Empataste esta ronda")
+                    break;
+            }
+            break;
+        }
+    }
+
+/*
 for (var i = 1; i <= rondas; i++) {
 
     player1 = parseInt(jugarPlayer1());
@@ -40,25 +79,33 @@ for (var i = 1; i <= rondas; i++) {
             switch (machine) {
                 case 0:     // Machine eligio TIJERA
                     winner = "Empate"
+                    alert("Empataste esta ronda");
                     break;
                 case 1:     // Machine eligio PAPEL
                     winner = "Player1"
+                    alert("Empataste esta ronda");
                     break;
                 case 2:     // Machine eligio PIEDRA
                     winner = "Machine"
+                    alert("Empataste esta ronda")
                     break;
             }
             break;
+
+
         case 1:         // Usuario eligio PAPEL
             switch (machine) {
                 case 0:     // Machine eligio TIJERA
                     winner = "Machine"
+                    alert("Empataste esta ronda");
                     break;
                 case 1:     // Machine eligio PAPEL
                     winner = "Empate"
+                    alert("Empataste esta ronda");
                     break;
                 case 2:     // Machine eligio PIEDRA
                     winner = "Player1"
+                    alert("Empataste esta ronda");
                     break;
             }
             break;
@@ -66,12 +113,15 @@ for (var i = 1; i <= rondas; i++) {
             switch (machine) {
                 case 0:     // Machine eligio TIJERA
                     winner = "Player1"
+                    alert("Empataste esta ronda");
                     break;
                 case 1:     // Machine eligio PAPEL
                     winner = "Machine"
+                    alert("Empataste esta ronda");
                     break;
                 case 2:     // Machine eligio PIEDRA
                     winner = "Empate"
+                    alert("Empataste esta ronda");
                     break;
             }
             break;
@@ -82,4 +132,7 @@ for (var i = 1; i <= rondas; i++) {
     document.write("<br>Machine : " + opciones[machine]);
     document.write("<br>El ganador es : " + winner);
 
+    
+
 }
+*/
